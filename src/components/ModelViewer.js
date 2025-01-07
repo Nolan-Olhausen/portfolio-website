302c48cb29e47263cc6c53bb5d0d12995ab53b53
Nolan-Olhausen <olhausennolan@gmail.com>
2025-01-07 02:05:43 -0700

    added basic 3d model and text structure

---
+++ src/components/ModelViewer.js
@@ -0,0 +1,27 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+
+const ModelViewer = ({ modelPath }) => {
+  const { scene } = useGLTF(modelPath);
+  console.log(scene);
+  return (
+    <Canvas>
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight
+        position={[10, 10, 10]}
+        angle={0.15}
+        penumbra={1}
+        decay={0}
+        intensity={Math.PI}
+      />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <mesh>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="hotpink" />
+      </mesh>
+    </Canvas>
+  );
+};
+
+export default ModelViewer;

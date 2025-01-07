8dc2ba548ecd6d7e0a7cac8b0970b51346cf9ef3
Nolan-Olhausen <olhausennolan@gmail.com>
2025-01-06 21:18:29 -0700

    Add Theme Context setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useContext } from 'react';
+import { ThemeContext } from './context/ThemeContext';
 
 function App() {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
+  // Set the theme in the HTML document's data-theme attribute
+  document.body.setAttribute('data-theme', theme);
+
   return (
     <div className="App">
       <header className="App-header">
+        <button onClick={toggleTheme}>Toggle Theme</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.

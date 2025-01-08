730d17edaf94e736504c483f42ad3d60c2742b85
Nolan-Olhausen <olhausennolan@gmail.com>
2025-01-08 16:57:51 -0700

    removed unused import

--- src/components/ModelViewer.js
+++ src/components/ModelViewer.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { useGLTF, OrbitControls } from "@react-three/drei";
 import { useErrorBoundary } from "use-error-boundary";

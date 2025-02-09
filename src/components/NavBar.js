87ae202a8867fdb5e299d034346cb0f772ac71ef
Nolan-Olhausen <olhausennolan@gmail.com>
2025-02-09 01:42:57 -0700

    icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,19 @@
 // src/components/NavBar.js
 import "../index.css";
 import React from "react";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 const NavBar = () => {
   return (
     <nav className="navbar">
-      <div>
+      <div className="navbar-left">
+        <div className="nav-icon">
+          <DotLottieReact
+            src="https://lottie.host/28ba67e4-afee-432d-8f26-ec9ffbced6c6/jGjMEPASkr.lottie"
+            loop
+            autoplay
+          />
+        </div>
         <h1>HAWKE . dev</h1>
       </div>
       <div className="navbar-right">
